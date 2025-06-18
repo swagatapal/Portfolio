f6aa74eb5103fc45b019c8d3adeaf9a695634b50
swagatapal <swagatapal1980@gmail.com>
2025-06-19 01:39:30 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,421 @@
+export const Bio = {
+  name: "Swagata Pal",
+  roles: [
+    "Senior Software Engineer",
+    "Flutter Developer",
+    "Android App Developer",
+    "Full Stack App Developer",
+    "Teacher",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/swagatapal",
+  resume:
+    "https://drive.google.com/file/d/1_e4PlZYp7yPuj_WjX5jATBtg0HtN4k-J/view?usp=share_link",
+  linkedin: "https://www.linkedin.com/in/swagata-pal-b1b0421a2/",
+  twitter: "",
+  insta: "",
+  facebook: "https://www.facebook.com/swagata.pal.9638/",
+};
+
+export const skills = [
+  {
+    title: "Hybrid App Development(Flutter)",
+    skills: [
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Dart",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOZdMC_FB3iUt9yQZujx9L8Etp5FdbP75UJQ&s",
+      },
+      {
+        name: "Firebase",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png",
+      },
+      {
+        name: "Riverpod",
+        image:
+          "https://riverpod.dev/img/cover.png",
+      },
+      {
+        name: "Bloc",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1128/1*N-tmEuiC6EiiYXE4KBCIng.png",
+      },
+      {
+        name: "Provider",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Play Store",
+        image:
+          "https://freelogopng.com/images/all_img/1664285914google-play-logo-png.png",
+      },
+      {
+        name: "App Store",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/App_Store_%28iOS%29.svg/2048px-App_Store_%28iOS%29.svg.png",
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "RestApi",
+        image:"https://www.opc-router.de/wp-content/uploads/2020/05/REST_socialmedia.jpg"
+      }
+    ],
+  },
+  {
+    title: "Android Development",
+    skills: [
+      {
+        name: "Kotlin Jetpack Compose",
+        image:
+          "https://developer.android.com/static/codelabs/jetpack-compose-animation/img/jetpack_compose_logo_with_rocket.png",
+      },
+      {
+        name: "Retrofit",
+               },
+      {
+        name: "Coroutines",
+                },
+      {
+        name: "ViewModel",
+                },
+      {
+        name: "LiveData",
+               },
+      {
+        name: "Room",
+               },
+      {
+        name: "WorkManager",
+               },
+      {
+        name: "Paging",
+                },
+      
+    ],
+  },
+  {
+    title: "Tools & Technologies",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      // {
+      //   name: "Docker",
+      //   image:
+      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      // },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+      {
+        name: "Android studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Android_Studio_icon_%282023%29.svg/2048px-Android_Studio_icon_%282023%29.svg.png",
+      },
+      {
+        name: "Xcode",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/5/56/Xcode_14_icon.png",
+      },
+      // {
+      //   name: "Adobe XD",
+      //   image:
+      //     "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      // },
+      // {
+      //   name: "Figma",
+      //   image:
+      //     "https://cdn.sanity.io/images/599r6htc/localized/46a76c802176eb17b04e12108de7e7e0f3736dc6-1024x1024.png?w=804&h=804&q=75&fit=max&auto=format",
+      // },
+      // {
+      //   name : "C",
+      //   image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/926px-C_Programming_Language.svg.png",
+      // },
+      // {
+      //   name: "Android Studio",
+      //   image:
+      //     "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Android_Studio_icon_%282023%29.svg/2048px-Android_Studio_icon_%282023%29.svg.png",
+      // },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://gdm-catalog-fmapi-prod.imgix.net/ProviderLogo/7f2b72de-8820-4e9f-9213-01e3069ffed7.png",
+    role: "Senior Software Engineer",
+    company: "Devant It Solution",
+    date: "jan 2025 - present",
+    desc: "Led full-cycle development of Flutter apps with clean, scalable architecture (Riverpod). Integrated REST APIs, payment gateways, and other third-party services. Built CI/CD pipelines to automate deployment (AppStore, PlayStore) Collaborated with cross-functional teams in fast-paced sprints and Mentored junior developers and code reviews. Optimized app performance and resolved production issues.",
+    skills: [
+      "Flutter",
+      "Riverpod",
+      "CI/CD Fastlane",
+      "App Deployment",
+    
+    ],
+    //doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 1,
+    img: "https://media.designrush.com/agencies/17558/conversions/Innofied-Solution-logo-profile.jpg",
+    role: "Associate Developer",
+    company: "Innofied Solution",
+    date: "Nov 2023 - jan 2025",
+    desc: "Building dynamic user interfaces,deep linking, Push notification, localization, third party login, Firebase messaging, In app purchase, API integration for seamless system connectivity.",
+    skills: [
+      "Flutter",
+      "Kotlin(Jetpack compose)",
+      "Rivarpod",
+      "Bloc",
+      "Dart",
+      "Firebase",
+      "Api Integration",
+      "deep linking", 
+      "Push notification",
+    ],
+    //doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 2,
+    img: "https://cdn.theorg.com/dda96733-6c54-4816-8f65-2d8787ddd236_medium.jpg",
+    role: "Associate Software Developer",
+    company: "Blu Cocoon Digital",
+    date: "June 2023 - Oct 2023",
+    desc: "Developing and maintaining internal mobile applications using Flutter.",
+    skills: [
+      "Flutter",
+      "NodeJs",
+      "ExpressJs",
+      "MongoDB",
+      "TypeScript",
+    ],
+    //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 3,
+    img: "https://static.vecteezy.com/system/resources/previews/004/610/354/non_2x/simple-minimalist-pen-graduation-hat-for-education-school-university-logo-design-vector.jpg",
+    role: "Teacher",
+    company: "Self Employed",
+    date: "June 2023 - Present",
+    desc: "Facilitated the learning of coding and computer science concepts for over 50 college students, providing mentorship and structured guidance to support their academic and professional growth.",
+    skills: [
+      "Online Teaching",
+      
+    ],
+    //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i0.wp.com/iemgroup.s3.amazonaws.com/uploads/sites/7/2020/10/cropped-logo-2.png?fit=426%2C250&ssl=1",
+    school: "Institute Of Engineering & Management, Kolkata (IEM)",
+    date: "Aug 2019 - May 2023",
+    grade: "9.28 CGPA",
+    desc: "I have successfully completed a four-year program in Computer Science and Engineering, obtaining a Bachelor's degree in Technology (B.Tech). During this time, I acquired a strong foundation in computer science principles and programming. Additionally, I have completed multiple projects that have further enriched my practical knowledge in the field.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://cache.careers360.mobi/media/article_images/2018/06/08/WBCHSE.jpg",
+    school: "Tajpur RC High School(HS), Bankura, West Bengal",
+    date: "March 2017 - April 2019",
+    grade: "86.5%",
+    desc: "I completed my class 12 high school education at Tajpur RC High School(HS), Bankura, West Bengal, where I studied Science with Biology.",
+    degree: "WBCHSE(XII), Science with Biology",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/en/d/d9/West_Bengal_Board_of_Secondary_Education_Logo.png",
+    school: "Tajpur RC High School(HS), Bankura, West Bengal",
+    date: "January 2016 - March 2017",
+    grade: "94%",
+    desc: "I completed my class 10 education at Tajpur RC High School(HS), Bankura, West Bengal, where I studied Science. I was the School topper in Secondery Examination",
+    degree: "WBBSE(X), Science",
+  },
+];
+
+export const projects = [
+  // {
+  //   id: 8,
+  //   title: "Home Service App",
+  //   date: "March 2024 - April 2024",
+  //   description:
+  //     "This is an application where you can book your daily regular basis home service related task", 
+  //   image:
+  //     "https://raw.githubusercontent.com/swagatapal/testImage/main/WhatsApp%20Image%202024-04-26%20at%2011.17.26%20PM.jpeg",
+  //   tags: [
+  //     "React Native",
+  //     "Hygraph",
+  //     "Clerk Authentication",
+  //     "Graphql",
+  //     "Javascript",
+  //     "Firebase",
+  //   ],
+  //   category: "android app",
+  //   github: "https://github.com/swagatapal/Home-Service-App",
+  //   webapp: "https://drive.google.com/file/d/1Pp6vWKh2wlghU1T2tver-8kvIfkV69Hk/view?usp=sharing",
+  // },
+  // {
+  //   id: 8,
+  //   title: "MarketPlace App",
+  //   date: "April 2024 - Present",
+  //   description:
+  //     "This is an application Where you can upload your product details, customer can view the product and buy the product and contact with the seller", 
+  //   image:
+  //     "https://raw.githubusercontent.com/swagatapal/testImage/main/WhatsApp%20Image%202024-04-26%20at%2011.29.19%20PM.jpeg",
+  //   tags: [
+  //     "React Native",
+  //     "Javascript",
+  //     "Clerk Authentication",
+  //     "Firebase",
+  //   ],
+  //   category: "android app",
+  //   github: "https://github.com/swagatapal/MarketPlace_App",
+  //   webapp: "https://drive.google.com/file/d/1YtNG2FxW1U8CD5bTIgvCWz6kCvH88q0k/view?usp=sharing",
+  // },
+  {
+    id: 8,
+    title: "Inspired Image",
+    date: "Jan 2024 - March 2024",
+    description:
+      "This is an application for photography lover. i have followed clean architechture, source code is not available due to restriction of company", 
+    image:
+      "https://raw.githubusercontent.com/swagatapal/testImage/main/WhatsApp%20Image%202024-04-26%20at%2010.31.48%20PM.jpeg",
+    tags: [
+      "Flutter",
+      "Dart",
+      "Bloc",
+      "Rest Api",
+      "Firebase",
+    ],
+    category: "flutter app",
+    github: "https://drive.google.com/file/d/19ZL0RclQ4NreQR369e89fYhBpxnj0-q5/view?usp=drive_link",
+    webapp: "https://drive.google.com/file/d/19ZL0RclQ4NreQR369e89fYhBpxnj0-q5/view?usp=drive_link",
+  },
+  // {
+  //   id: 8,
+  //   title: "E-Commerce App",
+  //   date: "Dec 2023 - jan 2024",
+  //   description:
+  //     "This is a complete E commerce application. Here Users side and seller side both are available. To developed this project i ham using Flutter framework(dart) and for database purpose i use Firebase. For state management i have used GETx controller and for better understanding i have followed clean architechture In this Project I have built all the Feautures for E commerce App",
+  //   image:
+  //     "https://github.com/swagatapal/e_commerce_app/blob/main/assets/images/e-cart-home.jpeg?raw=true",
+  //   tags: [
+  //     "Flutter",
+  //     "Dart",
+  //     "Firebase",
+  //   ],
+  //   category: "android app",
+  //   github: "https://github.com/swagatapal/e_commerce_app",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 9,
+  //   title: "Multi Disease Prediction",
+  //   date: "Jun 2023 - Jul 2023",
+  //   description:
+  //     "This is a multi disease prediction system where you can predict some diseases with good accuracy(>90%) and consult with near by doctor according to the disease , beside these here you will get the information about various kind of common disease and how to take precautions from those diseases and gain a brief knowledge about first aid.",
+  //   image:
+  //     "https://github.com/swagatapal/Apna-Clinic-main-complete-project/blob/main/mlProj.jpeg?raw=true",
+  //   tags: [
+  //     "Python",
+  //     "Machine Learning(RFC, KNN, SVM)",
+  //     "Deep Learning(CNN, TENSORFLOW, KERAS)",
+  //     "Flask",
+  //     "HTML",
+  //     "CSS",
+  //     "JavaScript",
+  //     "MySql",
+  //   ],
+  //   category: "machine learning",
+  //   github: "https://github.com/swagatapal/Apna-Clinic-main-complete-project",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 0,
+  //   title: "Blogging App",
+  //   date: "June 2023 - july 2023",
+  //   description:
+  //     "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+  //   image:
+  //     "https://gitu.net/gituimg/free-xd-templates/freebie-blog-components-for-adobe-xd-y4.jpg",
+  //   tags: ["Angular", "MongoDb", "Node Js", "Express Js", "Rest API"],
+  //   category: "web app",
+  //   github: "https://github.com/swagatapal123/Blog-Posting-App",
+  //   webapp: "",
+  //   // member: [
+  //   //   {
+  //   //     name: "Rishav Chanda",
+  //   //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+  //   //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  //   //     github: "https://github.com/rishavchanda/",
+  //   //   },
+  //   //   {
+  //   //     name: "Upasana Chaudhuri",
+  //   //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+  //   //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+  //   //     github: "https://github.com/upasana0710",
+  //   //   },
+  //   //],
+  // },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
